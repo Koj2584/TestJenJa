3664d3bb7dfbfe0ec1403a35d06ecb9c5a31eb8f
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-11-06 12:13:42 +0100

    Add files via upload

---
+++ TestJenJa/TestJenJa/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestJenJa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(191, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 152);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 257);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Prvocislo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestJenJa/TestJenJa/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestJenJa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains("*"))
+            {
+                try
+                {
+                    int cislo = int.Parse(textBox1.Text);
+                    if (JePrvocislo(cislo))
+                        label1.Text = "Je prvočíslo.";
+                    else
+                        label1.Text = "Není prvočíslo.";
+                }
+                catch(FormatException)
+                {
+                    MessageBox.Show("Musíte zadat číslo.");
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Příliš velké nebo malé číslo.");
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("Chyba. "+x.Message);
+                }
+                textBox1.Text = "";
+                textBox1.Focus();
+            } else
+            {
+                button1.Enabled = false;
+                textBox1.Text = "";
+                textBox1.Enabled = false;
+                label1.Text = "Konec posloupnosti.";
+            }
+        }
+
+        bool JePrvocislo(int x)
+        {
+            bool je = x > 1&&(x%2!=0||x==2);
+            for(int i = 3;i<=Math.Sqrt(x)&&je;i++)
+            {
+                je = x%i!=0;
+            }
+            return je;
+        }
+    }
+}
